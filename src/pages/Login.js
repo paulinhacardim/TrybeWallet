ec67763631cea730796a060ec8ea54e352bc718e
paulinha <paulinha@MacBook-Pro-de-seguranca.local>
2023-02-02 16:28:28 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,95 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
+// import { Link } from 'react-router-dom';
+// import { type } from '@testing-library/user-event/dist/type';
+import { dadosLogin } from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    senha: '',
+    email: '',
+    isDisabled: true,
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value }, this.validacaoEmail);
+  };
+
+  // A expressão [^0-9] é usada para localizar qualquer caractere que NÃO seja um dígito.
+  validacaoEmail = () => {
+    const { senha, email } = this.state;
+    const caracteres = 6;
+    // const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\)?$/;
+    const formatoEmail = (/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\)?$/).test(email);
+    if (formatoEmail && senha.length >= caracteres) {
+      this.setState({
+        isDisabled: false,
+      });
+      return;
+    }
+    this.setState({
+      isDisabled: true,
+    });
+  };
+
+  handleClick = () => {
+    const { dispatch, history } = this.props;
+    const { email } = this.state;
+    dispatch(dadosLogin(email));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    // return <div>Login</div>;
+
+    const { senha, email, isDisabled } = this.state;
+    return (
+      <div>
+        <label htmlFor="inputEmail">
+          <p>Email</p>
+          <input
+            data-testid="email-input"
+            id="inputEmail"
+            type="email"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+        </label>
+
+        <label htmlFor="inputPassword">
+          <p>Senha</p>
+          <input
+            data-testid="password-input"
+            id="inputPassword"
+            type="password"
+            name="senha"
+            value={ senha }
+            onChange={ this.handleChange }
+          />
+        </label>
+
+        {/* <Link to="/carteira"> */}
+        <button
+          type="button"
+          disabled={ isDisabled }
+          onClick={ () => this.handleClick() }
+        >
+
+          Entrar
+
+        </button>
+
+        {/* </Link> */}
+
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect()(Login);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,7 @@
 // Coloque aqui suas actions
+export const USER_FORM = 'USER_FORM';
+
+export const dadosLogin = (email) => ({
+  type: USER_FORM,
+  payload: email,
+});
